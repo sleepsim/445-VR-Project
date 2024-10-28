3bab70e0a81ad152cde30beb052826b842299dfb
sleepsim <91400544+sleepsim@users.noreply.github.com>
2024-10-27 22:33:31 -0700

    Added Kitchen + Raycast fixes

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR.LegacyInputHelpers;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using XRController = UnityEngine.XR.Interaction.Toolkit.XRController;
 
 public class CameraSwitch : MonoBehaviour
 {
     public Camera[] cameras;
     private int currentCameraIndex = 0;
+
+    public GameObject[] rigs;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +25,19 @@ public class CameraSwitch : MonoBehaviour
         if (index < 0 || index >= cameras.Length)
             return;
 
+        //Change current camera
         for (int i = 0; i < cameras.Length; i++)
         {
             cameras[i].gameObject.SetActive(i == index);
         }
+
+        //
+        for (int i = 0;i < rigs.Length; i++)
+        {
+            rigs[i].gameObject.SetActive(i == index);
+            //rigs[i].gameObject.GetComponentInChildren<XRController>().enabled = (i == index);
+        }
+
         currentCameraIndex = index;
     }
 

cf2060aa7f0587e464a46f3e5ad0c8da22474b88
sleepsim <91400544+sleepsim@users.noreply.github.com>
2024-11-24 03:46:39 -0800

    Ball activation changes
    Balls need to be activated by walking to them before being grabbable

--- Assets/Scripts/BallColourReveal.cs
+++ Assets/Scripts/BallColourReveal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class BallColourReveal : MonoBehaviour
 {
@@ -10,29 +11,37 @@ public class BallColourReveal : MonoBehaviour
     public Material defaultBallColour;
     public MeshRenderer ballRenderer;
 
+    XRGrabInteractable parentScript;
+
+    public void Start()
+    {
+       parentScript = GetComponentInParent<XRGrabInteractable>();
+    }
+
     public void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player") //If the collider is the player (toy) activate color and allow it to be grabbable
         {
             var materialsCopy = ballRenderer.materials;
             materialsCopy[0] = ballColour;
             ballRenderer.materials = materialsCopy;
+            parentScript.enabled = true;
         }
 
-        else {
-            var materialsCopy = ballRenderer.materials;
-            materialsCopy[0] = defaultBallColour;
-            ballRenderer.materials = materialsCopy;
-        }
+        //else {
+        //    var materialsCopy = ballRenderer.materials;
+        //    materialsCopy[0] = defaultBallColour;
+        //    ballRenderer.materials = materialsCopy;
+        //}
     }
 
     public void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            var materialsCopy = ballRenderer.materials;
-            materialsCopy[0] = defaultBallColour;
-            ballRenderer.materials = materialsCopy;
-        }
+        //if (other.tag == "Player")
+        //{
+        //    var materialsCopy = ballRenderer.materials;
+        //    materialsCopy[0] = defaultBallColour;
+        //    ballRenderer.materials = materialsCopy;
+        //}
     }
 }
